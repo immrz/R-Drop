dc68261ff0a21716555931558af0578cb6d84900
v-runmao <v-runmao@microsoft.com>
2021-09-03 15:16:20 +0800

    modified effnetv2 training script

--- vit_src/run_effnetv2m_cifar100.sh
+++ vit_src/run_effnetv2m_cifar100.sh
@@ -4,20 +4,34 @@ prt="True"
 ps=1
 pe=0.8
 drp=0.3
-tbs=128  # total bs = 128 * 4 = 512
-ebs=256
+
+# adjust batch size according to num of GPUs
+ngpu=${NPROC_PER_NODE:-1}
+let tbs=512/${ngpu}
+let gas=${tbs}/16
+ebs=128
 ee=100
-gas=8  # mbs = 128 / 8 = 16
 mt="efficientnetv2_m"
 ds="cifar100"
 nstep=10000
-ws=250
-lr=0.001
+ws=100
+lr=0.01
 wd=0
 alpha=1
 
-python -m torch.distributed.launch --nproc_per_node=4 train.py \
-    --pretrained ${prt} --model_type ${mt} --dataset ${ds} \
-    --num_steps ${nstep} --warmup_steps ${ws} --learning_rate ${lr} --weight_decay ${wd} \
-    --train_batch_size ${tbs} -gas ${gas} --eval_batch_size ${ebs} --eval_every ${ee} \
-    --prob_start ${ps} --prob_end ${pe} --alpha ${alpha} --dropout_prob ${drp} --fp16 $@
+echo "Using ${ngpu} GPUs"
+
+if [ ${ngpu} -gt 1 ]
+then
+    python -m torch.distributed.launch --nproc_per_node=${ngpu} train.py \
+        --pretrained ${prt} --model_type ${mt} --dataset ${ds} \
+        --num_steps ${nstep} --warmup_steps ${ws} --learning_rate ${lr} --weight_decay ${wd} \
+        --train_batch_size ${tbs} -gas ${gas} --eval_batch_size ${ebs} --eval_every ${ee} \
+        --prob_start ${ps} --prob_end ${pe} --alpha ${alpha} --dropout_prob ${drp} --fp16 $@
+else
+    python  train.py \
+        --pretrained ${prt} --model_type ${mt} --dataset ${ds} \
+        --num_steps ${nstep} --warmup_steps ${ws} --learning_rate ${lr} --weight_decay ${wd} \
+        --train_batch_size ${tbs} -gas ${gas} --eval_batch_size ${ebs} --eval_every ${ee} \
+        --prob_start ${ps} --prob_end ${pe} --alpha ${alpha} --dropout_prob ${drp} --fp16 $@
+fi
